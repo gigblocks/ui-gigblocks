e443042dfe89d74320aeb9b9acf86a723de6745f
Harkon666 <bryan.wicaksanaa@gmail.com>
2024-08-23 22:53:37 +0700

    feat: add basic modal component

---
+++ app/components/Modal/index.tsx
@@ -0,0 +1,32 @@
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function BasicModal(
+  { isOpen, handleClose, children }: Readonly<{ isOpen: boolean, handleClose: any, children: React.ReactNode }>
+) {
+
+  return (
+    <Modal
+      open={isOpen}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        {children}
+      </Box>
+    </Modal>
+  );
+}
